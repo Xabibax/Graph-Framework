e4097bda0004699af51ec35f21c52f2482cba8eb
Xabi <ale23@hotmail.fr>
2020-02-15 07:57:33 +0100

    explorerGraphe and explorerSommet implemented

--- src/main/java/Abstraction/IGraph.java
+++ src/main/java/Abstraction/IGraph.java
@@ -1,14 +1,48 @@
 package Abstraction;
 
+import Collection.Pair;
+import Nodes.AbstractNode;
+import Nodes.DirectedNode;
+import Nodes.UndirectedNode;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public interface IGraph {
-	/**
-	 * @return the number of nodes in the graph (referred to as the order of the graph)
- 	 */
-	int getNbNodes();
+    /**
+     * @return the number of nodes in the graph (referred to as the order of the graph)
+     */
+    int getNbNodes();
+
+    /**
+     * @return the adjacency matrix representation int[][] of the graph
+     */
+    int[][] toAdjacencyMatrix();
+
+    /**
+     * Method to explore every nodes of the graph
+     */
+    default void explorerGraphe() {
+    	Set<Integer> atteint = new HashSet<>();
+		for (int i = 0; i < this.toAdjacencyMatrix().length ; i++) {
+				if (!atteint.contains(i))
+					explorerSommet(i, atteint);
+		}
+        System.out.println(atteint);
+    }
 
 	/**
-	 * @return the adjacency matrix representation int[][] of the graph
- 	 */
-	int[][] toAdjacencyMatrix();
-
+	 * Method to explore a node and
+	 * @param s, a node index in the adjencyMatrix
+	 * @param a, a Set containing all previously explored nodes
+	 */
+	default void explorerSommet(Integer s, Set<Integer> a) {
+		a.add(s);
+		for (int i = 0; i < this.toAdjacencyMatrix()[s].length; i++) {
+			if (this.toAdjacencyMatrix()[s][i] > 0)
+				if (!a.contains(i))
+					explorerSommet(i, a);
+		}
+	}
 }
--- src/main/java/GraphAlgorithms/GraphToolsList.java
+++ src/main/java/GraphAlgorithms/GraphToolsList.java
@@ -14,124 +14,132 @@ import Collection.Triple;
 import Nodes.AbstractNode;
 import Nodes.DirectedNode;
 import Nodes.UndirectedNode;
+import sun.awt.UNIXToolkit;
 
-public class GraphToolsList  extends GraphTools {
+public class GraphToolsList extends GraphTools {
 
-	private static int _DEBBUG =0;
+    private static int _DEBBUG = 0;
 
-	private static int[] visite;
-	private static int[] debut;
-	private static int[] fin;
-	private static List<Integer> order_CC;
-	private static int cpt=0;
+    private static int[] visite;
+    private static int[] debut;
+    private static int[] fin;
+    private static List<Integer> order_CC;
+    private static int cpt = 0;
 
-	//--------------------------------------------------
-	// 				Constructors
-	//--------------------------------------------------
+    //--------------------------------------------------
+    // 				Constructors
+    //--------------------------------------------------
 
-	public GraphToolsList(){
-		super();
-	}
+    public GraphToolsList() {
+        super();
+    }
 
-	// ------------------------------------------
-	// 				Accessors
-	// ------------------------------------------
+    // ------------------------------------------
+    // 				Accessors
+    // ------------------------------------------
+
+
+    // ------------------------------------------
+    // 				Methods
+    // ------------------------------------------
+
+    /**
+     * @param graph, a graph
+     * @param start, start node
+     */
+    public static ArrayList<Boolean> parcourProfond(IGraph graph, AbstractNode start) throws Exception {
+        int n = graph.getNbNodes();
+        ArrayList<Boolean> mark = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            mark.add(false);
+        }
+        mark.set(start.getLabel(), true);
+        Stack<AbstractNode> toVisit = new Stack<AbstractNode>();
+        toVisit.push(start);
+        while (!toVisit.empty()) {
+            AbstractNode v = toVisit.pop();
+            ArrayList<AbstractNode> m = new ArrayList<>();
+            if (v.getClass() == UndirectedNode.class) {
+                for (Map.Entry<UndirectedNode, Integer> entry : ((UndirectedNode) v).getNeighbours().entrySet()) {
+                    m.add(entry.getKey());
+                }
+            } else if (v.getClass() == DirectedNode.class) {
+                for (Map.Entry<DirectedNode, Integer> entry : ((DirectedNode) v).getSuccs().entrySet()) {
+                    m.add(entry.getKey());
+                }
+            } else {
+                throw new Exception("Unknow node type");
+            }
+            m.forEach((node) -> {
+                if (!mark.get(node.getLabel())) {
+                    mark.set(node.getLabel(), true);
+                    toVisit.push(node);
+                }
+            });
+        }
+        return mark;
+    }
+
+    /**
+     * @param graph, a graph
+     * @param start, start node
+     */
+    public static ArrayList<Boolean> parcourEnLargeur(IGraph graph, AbstractNode start) throws Exception {
+        int n = graph.getNbNodes();
+        ArrayList<Boolean> mark = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            mark.add(false);
+        }
+        mark.set(start.getLabel(), true);
+        ConcurrentLinkedQueue<AbstractNode> toVisit = new ConcurrentLinkedQueue<>();
+        toVisit.add(start);
+        while (!toVisit.isEmpty()) {
+            AbstractNode v = toVisit.remove();
+            ArrayList<AbstractNode> m = new ArrayList<>();
+            if (v.getClass() == UndirectedNode.class) {
+                for (Map.Entry<UndirectedNode, Integer> entry : ((UndirectedNode) v).getNeighbours().entrySet()) {
+                    m.add(entry.getKey());
+                }
+            } else if (v.getClass() == DirectedNode.class) {
+                for (Map.Entry<DirectedNode, Integer> entry : ((DirectedNode) v).getSuccs().entrySet()) {
+                    m.add(entry.getKey());
+                }
+            } else {
+                throw new Exception("Unknow node type");
+            }
+            for (AbstractNode node : m) {
+                if (!mark.get(node.getLabel())) {
+                    mark.set(node.getLabel(), true);
+                    toVisit.add(node);
+                }
+            }
+        }
+        return mark;
+    }
 
 
 
-	// ------------------------------------------
-	// 				Methods
-	// ------------------------------------------
 
-	/**
-	 * @param graph, a graph
-	 * @param start, start node
-	 */
-	public static ArrayList<Boolean> parcourProfond(IGraph graph, AbstractNode start) throws Exception {
-		int n = graph.getNbNodes();
-		ArrayList<Boolean> mark = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			mark.add(false);
-		}
-		mark.set(start.getLabel(), true);
-		Stack<AbstractNode> toVisit = new Stack<AbstractNode>();
-		toVisit.push(start);
-		while (!toVisit.empty()) {
-			AbstractNode v = toVisit.pop();
-			ArrayList<AbstractNode> m = new ArrayList<>();
-			if (v.getClass() == UndirectedNode.class) {
-				for (Map.Entry<UndirectedNode, Integer> entry : ((UndirectedNode) v).getNeighbours().entrySet()) {
-					m.add(entry.getKey());
-				}
-			}else if (v.getClass() == DirectedNode.class)  {
-				for (Map.Entry<DirectedNode, Integer> entry : ((DirectedNode) v).getSuccs().entrySet()) {
-					m.add(entry.getKey());
-				}
-			} else {
-				throw new Exception("Unknow node type");
-			}
-			m.forEach((node) -> {
-				if (!mark.get(node.getLabel())) {
-					mark.set(node.getLabel(), true);
-					toVisit.push(node);
-				}
-			});
-		}
-		return mark;
-	}
-	/**
-	 * @param graph, a graph
-	 * @param start, start node
-	 */
-	public static ArrayList<Boolean> parcourEnLargeur(IGraph graph, AbstractNode start) throws Exception {
-		int n = graph.getNbNodes();
-		ArrayList<Boolean> mark = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			mark.add(false);
-		}
-		mark.set(start.getLabel(), true);
-		ConcurrentLinkedQueue<AbstractNode> toVisit = new ConcurrentLinkedQueue<>();
-		toVisit.add(start);
-		while (!toVisit.isEmpty()) {
-			AbstractNode v = toVisit.remove();
-			ArrayList<AbstractNode> m = new ArrayList<>();
-			if (v.getClass() == UndirectedNode.class) {
-				for (Map.Entry<UndirectedNode, Integer> entry : ((UndirectedNode) v).getNeighbours().entrySet()) {
-					m.add(entry.getKey());
-				}
-			}else if (v.getClass() == DirectedNode.class) {
-				for (Map.Entry<DirectedNode, Integer> entry : ((DirectedNode) v).getSuccs().entrySet()) {
-					m.add(entry.getKey());
-				}
-			} else {
-				throw new Exception("Unknow node type");
-			}
-			for (AbstractNode node: m) {
-				if (!mark.get(node.getLabel())) {
-					mark.set(node.getLabel(), true);
-					toVisit.add(node);
-				}
-			}
-		}
-		return mark;
-	}
+    public static void main(String[] args) throws Exception {
+        int[][] Matrix = GraphTools.generateGraphData(10, 20, false, false, true, 100001);
+        GraphTools.afficherMatrix(Matrix);
+        DirectedGraph<DirectedNode> al = new DirectedGraph<>(Matrix);
+        System.out.println(al);
 
-	public static void main(String[] args) throws Exception {
-		int[][] Matrix = GraphTools.generateGraphData(10, 20, false, false, true, 100001);
-		GraphTools.afficherMatrix(Matrix);
-		DirectedGraph<DirectedNode> al = new DirectedGraph<>(Matrix);
-		System.out.println(al);
-
-		for (Boolean aBoolean : parcourProfond(al, al.getNodeOfList(al.makeNode(1)))) {
-			System.out.print(parcourProfond(al, al.getNodeOfList(al.makeNode(1))).indexOf(aBoolean));
-			System.out.print(" ");
-			System.out.println(aBoolean);
-		}
-		for (Boolean r : parcourEnLargeur(al, al.getNodeOfList(al.makeNode(1)))) {
-			System.out.print(parcourProfond(al, al.getNodeOfList(al.makeNode(1))).indexOf(r));
-			System.out.print(" ");
-			System.out.println(r);
-		}
-
-	}
+        int node = 1;
+        System.out.println("Parours en profondeur du Graphe depuis le sommet " + node);
+        ArrayList<Boolean> bl = parcourProfond(al, al.getNodeOfList(al.makeNode(node)));
+        for (int i = 0; i < bl.size(); i++) {
+            System.out.print(i);
+            System.out.print(" ");
+            System.out.println(bl.get(i) ? "Parcouru" : "Non parcouru");
+        }
+        System.out.println("Parours en largeur du Graphe depuis le sommet " + node);
+        bl = parcourEnLargeur(al, al.getNodeOfList(al.makeNode(node)));
+        for (int i = 0; i < bl.size(); i++) {
+            System.out.print(i);
+            System.out.print(" ");
+            System.out.println(bl.get(i) ? "Parcouru" : "Non parcouru");
+        }
+    }
 }
